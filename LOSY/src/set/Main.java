41630256f248f5adc86886f6cc18288185edd037
AndresDPG <32043493+AndresDPG@users.noreply.github.com>
2017-11-25 09:48:32 -0500

    Prueba Commit server github

---
+++ LOSY/src/set/Main.java
@@ -0,0 +1,12 @@
+package set;
+
+import Interface.Join;
+import Interface.Profile_coordinator;
+
+public class Main {
+
+    public static void main(String args[]) {
+        //Join jfjoin = new Join();
+        Profile_coordinator coor = new Profile_coordinator();
+    }
+}
