13cc9bd1eb450d0c7ebaf7e2793b2edbb690b050
AndresDPG <32043493+AndresDPG@users.noreply.github.com>
2017-11-26 17:06:33 -0500

    ListMembers Test (Revisar BD).

--- LOSY/src/Form/ListEquipment.java
+++ LOSY/src/Form/ListEquipment.java
@@ -1,23 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Form;
 
 import Database_access.DaoEquipment;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
 
-/**
- *
- * @author famil
- */
 public class ListEquipment extends javax.swing.JPanel {
 
-    /**
-     * Creates new form ListEquipment
-     */
     public ListEquipment() {
         
         initComponents();
